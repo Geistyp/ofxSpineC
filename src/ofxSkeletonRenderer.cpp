aa629f0b1a0ba3407e51d82b8a7adc23ef9e36bd
Geistyp <geistyp@gmail.com>
2015-12-09 17:17:49 +0800

    error count

--- src/ofxSkeletonRenderer.cpp
+++ src/ofxSkeletonRenderer.cpp
@@ -1,10 +1,10 @@
 /******************************************************************************
  * Spine Runtimes Software License
  * Version 2.3
- * 
+ *
  * Copyright (c) 2013-2015, Esoteric Software
  * All rights reserved.
- * 
+ *
  * You are granted a perpetual, non-exclusive, non-sublicensable and
  * non-transferable license to use, install, execute and perform the Spine
  * Runtimes Software (the "Software") and derivative works solely for personal
@@ -16,7 +16,7 @@
  * or other intellectual property or proprietary rights notices on or in the
  * Software, including any copy thereof. Redistributions in binary or source
  * form must include this license and terms.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY ESOTERIC SOFTWARE "AS IS" AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
@@ -120,7 +120,7 @@ ofxSkeletonRenderer::ofxSkeletonRenderer(const char* skeletonDataFile, const cha
 	}
 
 	// Auto-skin , ignore default [0]
-	if (skeletonData->skinsCount > 0) 
+	if (skeletonData->skinsCount > 1) 
 		setSkin(skeletonData->skins[1]->name);
 
 	for (i = 0; i < skeletonData->animationsCount; ++i) {
@@ -354,7 +354,7 @@ bool ofxSkeletonRenderer::setAttachment (const char* slotName, const char* attac
 
 // --- CCBlendProtocol
 
-void ofxSkeletonRenderer::setBlendFunc(GLuint src, GLuint dst) 
+void ofxSkeletonRenderer::setBlendFunc(GLuint src, GLuint dst)
 {
 	blendFunc.src = src;
 	blendFunc.dst = dst;
